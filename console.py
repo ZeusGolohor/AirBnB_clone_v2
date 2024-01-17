7cc4804a0556dc6747c91877f750720787788a83
golohorzeus <golohorzeusufuoma@gmail.com>
2024-01-17 15:20:16 -0800

    Added create feature for all models

--- console.py
+++ console.py
@@ -144,10 +144,10 @@ class HBNBCommand(cmd.Cmd):
                     except ValueError:
                         pass
                 kwagrs[new_arg[0]] = new_arg[1]
-        new_instance = HBNBCommand.classes[args[0]](**kwagrs)
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+            new_instance = HBNBCommand.classes[args[0]](**kwagrs)
+            storage.save()
+            print(new_instance.id)
+            storage.save()
 
     def help_create(self):
         """ Help information for the create method """
