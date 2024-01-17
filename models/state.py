9b416de3111de9fd88d375afb00365730b3ac8b7
golohorzeus <golohorzeusufuoma@gmail.com>
2024-01-17 07:20:09 -0800

    Update the def do_create(self, arg): function of your command interpreter (console.py) to allow for object creation with given parameters:

--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models import storage
+from datetime import datetime
 
 
 class State(BaseModel):
-    """ State class """
-    name = ""
+    """
+    This class is used to manage the State class
+    instances.
+    """
+
+    def __init__(self, *args, **kwagrs):
+        """
+        This method is called wherever this class gets
+        instanciated.
+        """
+        if ("id" not in kwagrs.keys()):
+            super().__init__()
+        name = ""
+        for key, value in kwagrs.items():
+            if key != "__class__":
+                if (key == "created_at"):
+                    self.created_at = datetime.fromisoformat(value)
+                elif (key == "updated_at"):
+                    self.updated_at = datetime.fromisoformat(value)
+                else:
+                    setattr(self, key, value)
+        if ("id" not in kwagrs.keys()):
+            storage.new(self)
