417a18310885171a130bc606cdb80f7a4466b35a
golohorzeus <golohorzeusufuoma@gmail.com>
2024-01-17 16:02:36 -0800

    Write a script that prepares a MySQL server for the project:
    A database hbnb_dev_db
    A new user hbnb_dev (in localhost)
    The password of hbnb_dev should be set to hbnb_dev_pwd
    hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
    hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
    If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- creating database hbnb_dev_db if it does not exit
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- CREATE new user hbnb_dev (in localhost)
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- GRANTING PRIVILEGES
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
