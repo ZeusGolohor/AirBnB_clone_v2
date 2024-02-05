3763739a4d6b6ed09df356e3c08886e0f1e75c8a
root <zeusgolohor@gmail.com>
2024-02-04 18:51:28 -0800

    Write a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy:
    Prototype: def do_deploy(archive_path):
    Returns False if the file at the path archive_path doesn’t exist
    The script should take the following steps:
    Upload the archive to the /tmp/ directory of the web server
    Uncompress the archive to the folder /data/web_static/releases/<archive filename without extension> on the web server
    Delete the archive from the web server
    Delete the symbolic link /data/web_static/current from the web server
    Create a new the symbolic link /data/web_static/current on the web server, linked to the new version of your code (/data/web_static/releases/<archive filename without extension>)
    All remote commands must be executed on your both web servers (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
    Returns True if all operations have been done correctly, otherwise returns False
    You must use this script to deploy it on your servers: xx-web-01 and xx-web-02
    In the following example, the SSH key and the username used for accessing to the server are passed in the command line. Of course, you could define them as Fabric environment variables (ex: env.user =...)
    
    Disclaimer: commands execute by Fabric displayed below are linked to the way we implemented the archive function do_pack - like the mv command - depending of your implementation of it, you may don’t need it

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,21 +16,24 @@ def do_deploy(archive_path):
      to your web servers, using the function
     """
     if (os.path.exists(archive_path)):
-        f_name = archive_path.split('/')[-1]
-        name = f_name.split('.')[0]
-        store = '/data/web_static/releases/{}'.format(name)
-        rf_path = '/tmp/{}'.format(f_name)
-        l1 = '/data/web_static/current/web_static/*'
-        l2 = '/data/web_static/current'
-        run('mkdir -p /tmp')
-        run('mkdir -p {}'.format(store))
-        put(archive_path, '/tmp')
-        run('tar -xzvf {} -C {}'.format(rf_path, store))
-        run('rm -rf /tmp/{}'.format(f_name))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(store))
-        run('rsync -av --remove-source-files {} {}'.format(l1, l2))
-        run('rm -rf /data/web_static/current/web_static')
-        return (True)
+        try:
+            f_name = archive_path.split('/')[-1]
+            name = f_name.split('.')[0]
+            store = '/data/web_static/releases/{}'.format(name)
+            rf_path = '/tmp/{}'.format(f_name)
+            l1 = '/data/web_static/current/web_static/*'
+            l2 = '/data/web_static/current'
+            run('mkdir -p /tmp')
+            run('mkdir -p {}'.format(store))
+            put(archive_path, '/tmp')
+            run('tar -xzvf {} -C {}'.format(rf_path, store))
+            run('rm -rf /tmp/{}'.format(f_name))
+            run('rsync -av --remove-source-files {} {}'.format(l1, l2))
+            run('rm -rf /data/web_static/current/web_static')
+            run('rm -rf /data/web_static/current')
+            run('ln -s {} /data/web_static/current'.format(store))
+            return (True)
+        except Exception:
+            retrun (False)
     else:
         return (False)
