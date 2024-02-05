ab011c45dcd87bcfa9345918c153f1cda85d0cd8
root <zeusgolohor@gmail.com>
2024-02-04 19:42:13 -0800

    Checks completed: 0.0%) Write a Fabric script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy:
    Prototype: def deploy():
    The script should take the following steps:
    Call the do_pack() function and store the path of the created archive
    Return False if no archive has been created
    Call the do_deploy(archive_path) function, using the new path of the new archive
    Return the return value of do_deploy
    All remote commands must be executed on both of web your servers (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
    You must use this script to deploy it on your servers: xx-web-01 and xx-web-02
    In the following example, the SSH key and the username used for accessing to the server are passed in the command line. Of course, you could define them as Fabric environment variables (ex: env.user =…)

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""
+Write a Fabric script that generates a .tgz
+"""
+from fabric.api import (local)
+import tarfile
+from datetime import datetime
+
+import os
+# First we import the Fabric api
+from fabric.api import *
+
+env.hosts = ['35.175.135.174', '18.234.169.222']
+
+
+def do_pack():
+    """
+    used to create a tar file.
+    """
+    try:
+        folder = "web_static"
+        tyme = datetime.now().strftime("%Y%m%d%H%M%S")
+        tar_name = "versions/web_static_{}.tgz".format(tyme)
+        local('mkdir -p versions')
+        local('tar -cvzf {} web_static'.format(tar_name))
+        return (tar_name)
+    except Exception:
+        return (None)
+
+
+def do_deploy(archive_path):
+    """
+    Write a Fabric script (based on the file
+    1-pack_web_static.py) that distributes an archive
+     to your web servers, using the function
+    """
+    if (os.path.exists(archive_path)):
+        try:
+            f_name = archive_path.split('/')[-1]
+            name = f_name.split('.')[0]
+            store = '/data/web_static/releases/{}'.format(name)
+            rf_path = '/tmp/{}'.format(f_name)
+            l1 = '/data/web_static/current/web_static/*'
+            l2 = '/data/web_static/current'
+            run('mkdir -p /tmp')
+            run('mkdir -p {}'.format(store))
+            put(archive_path, '/tmp')
+            run('tar -xzvf {} -C {}'.format(rf_path, store))
+            run('rm -rf /tmp/{}'.format(f_name))
+            run('cp -r {}/web_static/* {}'.format(store, store))
+            run('rm -rf {}/web_static'.format(store))
+            run('rm -rf /data/web_static/current')
+            run('ln -s {} /data/web_static/current'.format(store))
+            return (True)
+        except Exception:
+            return(False)
+    else:
+        return (False)
+
+
+def deploy():
+    """
+    Write a Fabric script (based on the file
+     2-do_deploy_web_static.py) that creates and
+    distributes an archive to your web servers,
+    using the function deploy.
+    """
+    a_path = do_pack()
+    if (a_path is None):
+        return (None)
+    else:
+        res = do_deploy(a_path)
+        return (res)
