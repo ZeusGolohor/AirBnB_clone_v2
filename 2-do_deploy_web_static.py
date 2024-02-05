eaaa7ed20f79f55e8948144366a384a9e867d7df
root <zeusgolohor@gmail.com>
2024-02-04 18:12:12 -0800

    Write a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy:
    Prototype: def do_deploy(archive_path):
    Returns False if the file at the path archive_path doesn’t exist
    The script should take the following steps:
    Upload the archive to the /tmp/ directory of the web server
    Uncompress the archive to the folder /data/web_static/releases/<archive filename without extension> on the web server
    Delete the archive from the web server
    Delete the symbolic link /data/web_static/current from the web server
    Create a new the symbolic link /data/web_static/current on the web server, linked to the new version of your code (/data/web_static/releases/<archive filename without extension>)
    All remote commands must be executed on your both web servers (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
    Returns True if all operations have been done correctly, otherwise returns False
    You must use this script to deploy it on your servers: xx-web-01 and xx-web-02
    In the following example, the SSH key and the username used for accessing to the server are passed in the command line. Of course, you could define them as Fabric environment variables (ex: env.user =...)
    
    Disclaimer: commands execute by Fabric displayed below are linked to the way we implemented the archive function do_pack - like the mv command - depending of your implementation of it, you may don’t need it

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-Script used to deploy an archive.
+Scrit used to Deploy archive!
 """
 import os
+# First we import the Fabric api
 from fabric.api import *
 
 env.hosts = ['35.175.135.174', '18.234.169.222']
@@ -10,50 +11,26 @@ env.hosts = ['35.175.135.174', '18.234.169.222']
 
 def do_deploy(archive_path):
     """
-    Distributes an archive to web servers.
-
-    :param archive_path: Path to the archive file.
-    :return: True if all operations are done correctly, False otherwise.
+    Write a Fabric script (based on the file
+    1-pack_web_static.py) that distributes an archive
+     to your web servers, using the function
     """
-    if not os.path.exists(archive_path):
-        print(f"Error: Archive not found at {archive_path}")
-        return False
-
-    try:
-        # Extract necessary information from the archive_path
-        archive_name = os.path.basename(archive_path)
-        archive_no_ext = os.path.splitext(archive_name)[0]
-
-        # Define paths for deployment
-        store_path = '/data/web_static/releases/{}'.format(archive_no_ext)
-        remote_archive_path = '/tmp/{}'.format(archive_name)
-
-        # Create necessary directories
-        run('mkdir -p {}'.format(store_path))
+    if (os.path.exists(archive_path)):
+        f_name = archive_path.split('/')[-1]
+        name = f_name.split('.')[0]
+        store = '/data/web_static/releases/{}'.format(name)
+        rf_path = '/tmp/{}'.format(f_name)
+        l1 = '/data/web_static/current/web_static/*'
+        l2 = '/data/web_static/current'
         run('mkdir -p /tmp')
-
-        # Upload the archive to /tmp/ directory on the web server
+        run('mkdir -p {}'.format(store))
         put(archive_path, '/tmp')
-
-        # Uncompress the archive to the specified folder
-        run('tar -xzvf {} -C {}'.format(remote_archive_path, store_path))
-
-        # Remove the uploaded archive from /tmp/
-        run('rm -rf {}'.format(remote_archive_path))
-
-        # Remove the existing symbolic link
+        run('tar -xzvf {} -C {}'.format(rf_path, store))
+        run('rm -rf /tmp/{}'.format(f_name))
         run('rm -rf /data/web_static/current')
-
-        # Create a new symbolic link
-        run('ln -s {} /data/web_static/current'.format(store_path))
-
-        # Sync contents and remove source files
-        run('rsync -av --remove-source-files {}/web_static/ \
-        /data/web_static/current/'.format(store_path))
-
-        print("Deployment successful!")
-        return True
-
-    except Exception as e:
-        print(f"Error during deployment: {e}")
-        return False
+        run('ln -s {} /data/web_static/current'.format(store))
+        run('rsync -av --remove-source-files {} {}'.format(l1, l2))
+        run('rm -rf /data/web_static/current/web_static')
+        return (True)
+    else:
+        return (False)
