a7f4237f1a6d369a06aca9440fcbd53c9e8ad03e
root <zeusgolohor@gmail.com>
2024-02-05 17:26:19 -0800

    Write a Fabric script (based on the file 3-deploy_web_static.py) that deletes out-of-date archives, using the function do_clean:
    Prototype: def do_clean(number=0):
    number is the number of the archives, including the most recent, to keep.
    If number is 0 or 1, keep only the most recent version of your archive.
    if number is 2, keep the most recent, and second most recent versions of your archive.
    etc.
    Your script should:
    Delete all unnecessary archives (all archives minus the number to keep) in the versions folder
    Delete all unnecessary archives (all archives minus the number to keep) in the /data/web_static/releases folder of both of your web servers
    All remote commands must be executed on both of your web servers (using the env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
    In the following example, the SSH key and the username used for accessing to the server are passed in the command line. Of course, you could define them as Fabric environment variables (ex: env.user =…)
    
    guillaume@ubuntu:~/AirBn

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -13,11 +13,18 @@ def do_clean(number=0):
     Delete all unnecessary archives (all archives minus the number to keep)
     """
     if (number == '1' or number == '0' or number == 0):
-        local('ls -1tr versions/ | grep -v "total" | head -n -1 |  xargs -I {} rm -rf versions/{}')
-        run('ls -1tr /data/web_static/releases | grep -v "total" | head -n -1 |  xargs -I {} rm -rf /data/web_static/releases/{}')
+        local('ls -1tr versions/ | grep -v "total" | head -n -1 |  \
+            xargs -I {} rm -rf versions/{}')
+        run('ls -1tr /data/web_static/releases | grep -v "total" | \
+            head -n -1 |  xargs -I {} rm -rf /data/web_static/releases/{}')
     elif (number == '2'):
-        local('ls -1tr versions/ | grep -v "total" | head -n -2 |  xargs -I {} rm -rf versions/{}')
-        run('ls -1tr /data/web_static/releases | grep -v "total" | head -n -2 |  xargs -I {} rm -rf /data/web_static/releases/{}')
+        local('ls -1tr versions/ | grep -v "total" | head -n -2 |  \
+            xargs -I {} rm -rf versions/{}')
+        run('ls -1tr /data/web_static/releases | grep -v "total" | \
+            head -n -2 |  xargs -I {} rm -rf /data/web_static/releases/{}')
     else:
-        local('ls -1tr versions/ | grep -v "total" | head -n -{} |'.format(number) + '  xargs -I {} rm versions/{}')
-        run('ls -1tr /data/web_static/releases | grep -v "total" | head -n -{} |'.format(number) + '  xargs -I {} rm /data/web_static/releases/{}')
+        local('ls -1tr versions/ | grep -v "total" | head -n -{} \
+            |'.format(number) + '  xargs -I {} rm versions/{}')
+        run('ls -1tr /data/web_static/releases | grep -v "total" \
+            | head -n -{} |'.format(number) + '  xargs -I {} rm \
+            /data/web_static/releases/{}')
