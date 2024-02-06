a0cbc028d33a52a50b5e5c455a7c0722dcdbefe9
root <zeusgolohor@gmail.com>
2024-02-05 16:21:07 -0800

    Write a Fabric script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy:
    Prototype: def deploy():
    The script should take the following steps:
    Call the do_pack() function and store the path of the created archive
    Return False if no archive has been created
    Call the do_deploy(archive_path) function, using the new path of the new archive
    Return the return value of do_deploy
    All remote commands must be executed on both of web your servers (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
    You must use this script to deploy it on your servers: xx-web-01 and xx-web-02
    In the following example, the SSH key and the username used for accessing to the server are passed in the command line. Of course, you could define them as Fabric environment variables (ex: env.user =…)

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,7 +12,7 @@ def do_clean(number=0):
     """
     Delete all unnecessary archives (all archives minus the number to keep)
     """
-    if (number == '1'):
+    if (number == '1' or number == '0' or number == 0):
         local('ls -t /versions | tail -n +2 | xargs rm -rf')
         run('ls -t /data/web_static/releases | tail -n +2 | xargs rm -rf')
     elif (number == '2'):
