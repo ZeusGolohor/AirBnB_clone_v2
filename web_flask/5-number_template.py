f1b609dbf95541d1a4a06131ea2387ee5a06490e
golohorzeus <golohorzeusufuoma@gmail.com>
2024-02-19 04:33:43 -0800

    Write a script that starts a Flask web application:
    Your web application must be listening on 0.0.0.0, port 5000
    Routes:
    /hbnb: display “HBNB”
    /c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
    /python/(<text>): display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
    The default value of text is “is cool”
    /number/<n>: display “n is a number” only if n is an integer
    /number_template/<n>: display a HTML page only if n is an integer:
    H1 tag: “Number: n” inside the tag BODY
    You must use the option strict_slashes=False in your route definition

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+"""
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Defines the index method for the app.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Defines the hbnb method for the app.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    Defines the hbnb method for the app.
+    """
+    pro = text.replace('_', ' ')
+    return f"C {pro}"
+
+
+@app.route('/python')
+@app.route('/python/<text>', strict_slashes=False)
+def pyt(text='is cool'):
+    """
+    Defines the python route.
+    """
+    pro_text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def num(n):
+    try:
+        n = int(n)
+        return f'{n}'
+    except Exception:
+        pass
+
+
+@app.route('/number_template/<n>', strict_slashes=False)
+def num_temp(n):
+    try:
+        n = int(n)
+        return render_template('5-number.html', n=n)
+    except Exception:
+        pass
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
