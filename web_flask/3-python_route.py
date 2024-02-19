ca4c5b6e75a82d8b6e4f5699d6c8408827ae370c
golohorzeus <golohorzeusufuoma@gmail.com>
2024-02-19 03:51:21 -0800

    Write a script that starts a Flask web application:
    Your web application must be listening on 0.0.0.0, port 5000
    Routes:
    /hbnb: display “HBNB”
    /c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
    /python/<text>: display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
    The default value of text is “is cool”
    You must use the option strict_slashes=False in your route definition

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Defines the index method for the app.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Defines the hbnb method for the app.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    Defines the hbnb method for the app.
+    """
+    pro = text.replace('_', ' ')
+    return f"C {pro}"
+
+
+@app.route('/python')
+@app.route('/python/<text>', strict_slashes=False)
+def pyt(text='is cool'):
+    """
+    Defines the python route.
+    """
+    pro_text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
